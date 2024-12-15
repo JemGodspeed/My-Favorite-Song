27d271dcb8a49586177111384da2c9d4734a9589
JemGodspeed <cemkadir_92@hotmail.com>
2024-12-14 19:24:28 -0500

    Add project files.

---
+++ My Favorite Song/wwwroot/js/site.js
@@ -0,0 +1,5 @@
+﻿document.getElementById("animateBtn").addEventListener("click", function () {
+    const content = document.getElementById("content");
+    content.style.transition = "transform 2s";
+    content.style.transform = "scale(1.2)";
+});
